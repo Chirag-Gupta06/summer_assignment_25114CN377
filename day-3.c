d872b65a471f731fb7b477b2a83a41ff534724a9
Chirag  Gupta <chiraggupta90906@gmail.com>
2026-05-30 18:11:11 +0530

    day-3uploaded.

---
+++ day-3.c
@@ -0,0 +1,123 @@
+//program 1
+//Write a program to Check whether a number is prime.
+// #include<stdio.h>
+// int main(){
+//     int n,c=0;
+//     printf("enter a number");
+//     scanf("%d",&n);
+//     for(int i=1;i<=n;i++){
+//         if(n%i==0){
+//             c++;
+//         }
+        
+
+//     }if(c>2){
+//         printf("entered number is not prime");
+//     }
+//     else
+//     printf("enterd number is prime");
+    
+// }
+
+
+
+//program 2
+//Write a program to Print prime numbers in a range.
+// #include<stdio.h>
+// int main(){
+//     int n,n1,c=0,c1=0;
+//     printf("enter a range");
+//     scanf("%d",&n);
+//     scanf("%d",&n1);
+//     printf("\nthe entered range is from %d to %d",n,n1);
+    
+//     for(int i=n;i<=n1;i++){
+//         c=0;
+//         for (int j=1;j<=i;j++){
+//             if(i%j==0){
+//                 c++;
+//             }}
+//              if(c==2)
+//                 c1++;
+//                 printf("\nprime numbers are:- %d",i);
+            
+            
+        
+//     }
+//     printf("total prime numbers are:-%d",c1);
+// }
+
+
+
+
+// program 3
+// Write a program to Find GCD of two numbers
+// #include<stdio.h>
+// int main(){
+//     int n,n1,c;
+//     printf("enter two numbers : ");
+//     scanf("%d %d",&n,&n1);
+//    for(int i=1;i<=n;i++){
+//     int a=0,b=0;
+//     if(n%i==0){
+//         a=i;
+//         for(int j=1;j<=n1;j++){
+//             if(n1%j==0){
+//                 b=j;
+//                 if (a==b){
+//                     c=a;
+//                 }
+//             }
+//         }
+//     }
+    
+//    }
+//    printf("the GCD of two numbers are: %d",c);
+   
+
+// }
+
+
+//program 4
+//Write a program to Find LCM of two numbers.
+// #include<stdio.h>
+// int main(){
+//     int n,n1,c;
+//     printf("enter two numbers : ");
+//     scanf("%d %d",&n,&n1);
+//    for(int i=1;i<=n;i++){
+//     int a=0,b=0;
+//     if(n%i==0){
+//         a=i;
+//         for(int j=1;j<=n1;j++){
+//             if(n1%j==0){
+//                 b=j;
+//                 if (a==b){
+//                     c=a*b;
+//                 }
+//             }
+//         }
+//     }
+    
+//    }
+//    printf("the GCD of two numbers are: %d",c);
+   
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
