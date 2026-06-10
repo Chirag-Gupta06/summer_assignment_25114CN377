229c856e32765a72f7b660b4ba5ea44921911ce0
Chirag  Gupta <chiraggupta90906@gmail.com>
2026-06-10 11:16:01 +0530

    day-10uploaded.

--- day-10/program2.c
+++ day-10/program2.c
@@ -1,27 +1,31 @@
 /*
-Write a program to Print star pyramid. 
-    * 
-   *** 
-  ***** 
- ******* 
+Write a program to Print reverse pyramid. 
 ********* 
-
+ ******* 
+  ***** 
+   *** 
+    * 
 */
+
 #include<stdio.h>
 int main(){
     int n,a=1;
     printf("enter the number of rows");//assume rows=5
     scanf("%d",&n);
-    for(int i=n;i>=1;i--){
+    for(int i=1;i<=n;i++){
+        a=-1;
         printf("\n");
-        for(int j=i-1;j>=1;j--){
+        for(int j=1;j<=i-1;j++){
             printf(" ");
+        }
+        for(int m=i;m<=n;m++){
+            a+=2;
+
         }
         for(int k=1;k<=a;k++){
         printf("*");
         
     }
-    a=a+2;//for [printing *]
         
 
 }
